b2f3187dce3d26fe641e304f5de0a8e418941e8e
Ian Flaherty <ianrocket@gmail.com>
2015-05-26 12:14:07 -0700

    lots of changes where had

--- CalculatorPhysics/app/src/main/java/com/example/ian_flaherty/calculatorphysics/VarEntryList.java
+++ CalculatorPhysics/app/src/main/java/com/example/ian_flaherty/calculatorphysics/VarEntryList.java
@@ -3,32 +3,27 @@ package com.example.ian_flaherty.calculatorphysics;
 import android.app.Activity;
 import android.widget.ListView;
 import android.os.Bundle;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import android.widget.ArrayAdapter;
 
 public class VarEntryList extends Activity {
 
-    private ListView lv;
-
     public void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_var_entry);
 
-        lv = (ListView) findViewById(R.id.VarEntryList);
+        popListView();
+    }
+    private void popListView(){
+        String[] myItems = {"one","two","three"};
 
-        // Instanciating an array list (you don't need to do this,
-        // you already have yours).
-        ArrayList<String> your_array_list = new ArrayList<String>();
-        your_array_list.add("first var");
-        your_array_list.add("second var");
 
-        // This is the array adapter, it takes the context of the activity as a
-        // first parameter, the type of list view as a second parameter and your
-        // array as a third parameter.
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                your_array_list );
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.listbox,myItems);
 
-        lv.setAdapter(arrayAdapter);
+
+        ListView list = (ListView) findViewById(R.id.VarEntryList);
+        list.setAdapter(adapter);
     }
 }
